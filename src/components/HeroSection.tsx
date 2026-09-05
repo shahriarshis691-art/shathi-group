5209652f0ec49be9f40949d41dceebf3c93f8627
shahriarshis691-art <shahriarshis691@gmail.com>
2026-09-05 14:28:46 +0600

    style: remove bottom white gradient fog from hero image overlay

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -33,7 +33,7 @@ export function HeroSection() {
         />
       </ParallaxContainer>
 
-      <div aria-hidden="true" className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/40 to-white z-10" />
+      <div aria-hidden="true" className="pointer-events-none absolute inset-0 z-10 bg-black/40" />
 
       <div className="relative z-20 w-full">
         <div className="container-corporate">
